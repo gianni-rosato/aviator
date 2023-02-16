2744e3b3bfaa81d0ae0e0424c8808e5318ebadf4
Gianni Rosato <giannirosato@proton.me>
2023-02-16 13:33:39 -0500

    fixed subtitle & audio mapping to only happen when there's audio or subs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Aviator',
-    version='0.2.1',
+    version='0.2.1a',
     description='AV1/OPUS Encoder GUI',
     author='Gianni Rosato',
     author_email='grosatowork@proton.me',
